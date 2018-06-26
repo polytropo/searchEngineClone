1e4f88ef6270e134fc59e4635a162838c3df6ee1
Primoz <primoz969@gmail.com>
2018-06-26 09:43:06 +0200

    styling main page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to my searchEngine</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="wrapper indexPage">
+		<div class="mainSection">
+			<div class="logoContainer">
+				<img src="assets/images/doodleLogo.png" alt="Logo image">
+			</div>
+
+			<div class="searchContainer">
+				<form action="search.php" method="get">
+					<input class="searchBox" type="text" name="term" placeholder="Enter a search term...">
+					<input class="searchButton" type="submit" value="Search Now">
+				</form>		
+			</div> 
+		</div>
+	</div>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to my searchEngine</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<div class="wrapper">
+		<div class="header">
+			<div class="headerContent">
+				<div class="logoContainer">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
